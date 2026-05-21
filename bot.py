e640a079d23989ebfc8104982041d6164ca831cf
zxcgepard <mironromanuk3@gmail.com>
2026-05-21 21:39:19 +0300

    update bot

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ import requests
 
 BOT_TOKEN = '8172308599:AAEuJ9Zd3vVETx18Ozi6fGuEMJ60cGWDmvk'
 bot = telebot.TeleBot(BOT_TOKEN)
-
 def menu():
     kb = ReplyKeyboardMarkup(resize_keyboard=True)
     kb.add(
@@ -15,16 +14,28 @@ def menu():
     )
     return kb
 
+food = ['лук', 'картошка', 'морковь', 'свекла', 'капуста', 'мясо', 'курица',
+        'яйца', 'молоко', 'сыр', 'масло', 'мука', 'сахар', 'соль', 'помидоры',
+        'огурцы', 'чеснок', 'рыба', 'гречка', 'рис', 'макароны']
+
+def has_food(text):
+    text = text.lower()
+    for item in food:
+        if item in text:
+            return True
+        return False
+
 def get_recipe(q):
     try:
         r = requests.post('http://localhost:11434/api/generate', json={
             "model": "qwen2.5:3b",
-            "prompt": f"Ты шеф-повар. Напиши рецепт на русском языке: {q}. Название, ингредиенты, шаги.",
+            "prompt": f"Напиши рецепт на русском языке: {q}. Название, ингредиенты, шаги.",
             "stream": False
         })
-        return r.json()['response']
-    except:
-        return "Ошибка! Проверь, запущена ли Ollama"
+        raw_recipe = r.json()['response']
+        return clean_markdown(raw_recipe)
+    except Exception as e:
+        return f"Ошибка: {e}"
 
 @bot.message_handler(commands=['start'])
 def start(msg):
@@ -32,28 +43,57 @@ def start(msg):
 
 @bot.message_handler(func=lambda m: m.text == 'На ужин')
 def dinner(m):
+    bot.send_message(m.chat.id, "Секунду, процесс может занять где-то 1 минуты")
     bot.send_message(m.chat.id, get_recipe("ужин из простых продуктов"))
 
 @bot.message_handler(func=lambda m: m.text == 'Десерт')
 def dessert(m):
+    bot.send_message(m.chat.id, "Секунду, процесс может занять где-то 1 минуты")
     bot.send_message(m.chat.id, get_recipe("простой десерт"))
 
 @bot.message_handler(func=lambda m: m.text == 'Случайный')
 def random_recipe(m):
+    bot.send_message(m.chat.id, "Секунду, процесс может занять где-то 1 минуты")
     bot.send_message(m.chat.id, get_recipe("любое блюдо"))
 
 @bot.message_handler(func=lambda m: m.text == 'Ввести словами')
 def ask_products(m):
-    bot.send_message(m.chat.id, "Напиши продукты через запятую")
+    bot.send_message(m.chat.id, "Напиши продукты через запятую, например: картошка, лук, яйца")
 
 @bot.message_handler(func=lambda m: True)
-def handle_text(m):
-    if m.text.startswith('/'):
+def handle_text(msg):
+    if msg.text.startswith('/'):
         return
-    if m.text in ['На ужин', 'Десерт', 'Случайный', 'Ввести словами']:
-        return
-    bot.send_message(m.chat.id, get_recipe(m.text))
+    if has_food(msg.text):
+        bot.send_message(msg.chat.id, f"Секунду, процесс может занять где-то 1 минуты .Ищу рецепт из: {msg.text}...")
+        bot.send_message(msg.chat.id, get_recipe(msg.text))
+    else:
+        bot.send_message(msg.chat.id, "Я понимаю только продукты. Пример: картошка, лук, яйца", reply_markup=menu())
+
+
+
+
+import re
+
+def clean_recipe(text):
+    text = text.replace('*', '')
+    english_words = ['Dill', 'Bay leaf', 'sour cream', 'épaissit', 'chicken', 'beef', 'apple', 'pepper', 'salt', 'sugar', 'milk', 'butter', 'egg', 'cheese', 'cream', 'water', 'pimienta', 'Olivenöl', 'Oliven', 'öl']
+    for word in english_words:
+       text = text.replace(word, '')
+    text = re.sub(r'\s+', ' ', text)
+    return text.strip()
+
+
+import re
+
+def clean_markdown(text):
+    text = re.sub(r'\*\*(.*?)\*\*', r'\1', text)
+    text = re.sub(r'\*(.*?)\*', r'\1', text)
+    text = re.sub(r'__(.*?)__', r'\1', text)
+    text = re.sub(r'_(.*?)_', r'\1', text)
+    text = re.sub(r'^\*\s+', '- ', text, flags=re.MULTILINE)
+    return text
 
 if __name__ == '__main__':
-    print("Бот запущен")
+    print("Бот запущен...")
     bot.polling(none_stop=True)
